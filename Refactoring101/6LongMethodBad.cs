e7bc23664fa20a6cbfa787a78304202e424324f1
Thnpat <thnpat.wir@gmail.com>
2026-01-23 16:42:13 +0700

    Lab2

---
+++ Refactoring101/6LongMethodBad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactoring101;
+
+public class LongMethod
+{
+    // A very long method
+    public void PrintReport()
+    {
+        PrintHeader();
+        PrintBody();
+        PrintFooter();
+    }
+    private void PrintHeader()
+    {
+        // ...........
+    }
+    private void PrintBody()
+    {
+        // ...........
+    }
+    private void PrintFooter()
+    {
+        // ...........
+    }
+}
---
+++ Refactoring101/QuestionsAndAnswers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactoring101
+{
+    // Do Refactoring the following code:
+    public class QuestionsAndAnswers
+    {
+        // 1. Mysterious Name
+        public double Calc(double a, double b)
+        {
+            return a > b ? a : b;
+        }
+
+        public void Print()
+        {
+            string name = "   Mr.Harry Potter";
+
+            PrintSection();
+            PrintSection();
+            PrintSection();
+        }
+
+        private void PrintSection()
+        {
+            Console.WriteLine("***********************");
+            PrintBody();
+            Console.WriteLine("***********************");
+            Console.WriteLine();
+        }
+
+        private void PrintBody()
+        {
+            Console.WriteLine("...");
+        }
+
+    }
+
+    // 3. Shotgun Surgery
+    public class Shotgun1
+        {
+            public void DisplayStudents()
+            {
+                Console.WriteLine("Student Count = " + 48);
+            }
+        }
+        public class Shotgun2
+        {
+            public void PrintTotal()
+            {
+                Console.WriteLine("Total Students : " + 48);
+            }
+        
+
+        // 4. Data Clump
+        public void PrintDate(int day, int month, int year)
+        {
+            Console.WriteLine($"{day:00}/{month:00}/{year:0000}");
+        }
+        // 5. Feature Envy
+        //     จากข้อที่แล้ว น่าจะได้สร้างคลาส Date ขึ้นมา
+        //     ในคลาส Date นั้นให้สร้าง method: public string Format()
+        //      ปรับให้ PrintDate(...) ของเดิม ไปเรียก date.Format() ดังกล่าว
+    }
+}
